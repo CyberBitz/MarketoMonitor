2af10ebda06df0d9b1ee59d472785c674c59edca
Thomas Alan Jenkins <thomasajenkins@outlook.com>
2021-06-14 08:51:43 -0300

    added color and status messages

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 # https://github.com/CyberBitz/MarketoMonitor
 
 import requests
+from os import system, name
 import os
 from bs4 import BeautifulSoup
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+        
 marketto_orgs = []
 marketto_orgs_new = []
 if not os.path.exists('marketo_orgs.txt'):
@@ -21,8 +28,22 @@ pagecount = 1
 pages = []
 orgs = []
 
+class bcolors:
+    STATUS = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    NORMAL = '\033[94m'
+    
+clear()
+print(f"{bcolors.NORMAL}Receiving Page: {bcolors.ENDC}")
+def receivingPages():
+    print(f"{bcolors.STATUS}" + str(pagecount) + f" {bcolors.ENDC}", end="", flush=True)
+
 while valid > 0:
     url = 'https://marketo.cloud/?page=' + str(pagecount)
+    
+    receivingPages()
+
     try:
         response = requests.get(url, verify=False)
         if response.status_code != 200:
@@ -58,10 +79,14 @@ for oz in orgs:
 file_object = open('marketo_orgs.txt', 'a')
 for norg in marketto_orgs_new:
     file_object.write(norg+"\n")
-
-print("Recorded:") 
+print("\n================================")
+print(f"{bcolors.STATUS}Existing Orgs: " + str(len(marketto_orgs)) + f"{bcolors.STATUS}", end="")
+print(f"{bcolors.NORMAL}")
 print(marketto_orgs)
-print("New:")
-print(marketto_orgs_new) # print array of new orgs posted
+print(f"{bcolors.NORMAL}")
+print(f"{bcolors.STATUS}New Orgs: " + str(len(marketto_orgs_new)) + f"{bcolors.STATUS}", end="")
+print(f"{bcolors.NORMAL}")
+print(marketto_orgs_new)
+print(f"{bcolors.NORMAL}")# print array of new orgs posted
 # do any alerting here, I implement a telegram bot I push the new orgs that have been added.
 
